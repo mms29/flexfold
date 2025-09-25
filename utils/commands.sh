06b9488ea32ee3a778c0e0a1048ea54b6e9e6e01
Remi Vuillemot Remi.Vuillemot@univ-grenoble-alpes.fr <vuillemr@>
2025-09-25 08:33:56 +0200

    wip

--- utils/commands.sh
+++ utils/commands.sh
@@ -478,32 +478,36 @@ python ./scripts/compare_traj.py "$RUN_DIR/analysis/traj/*pdb" "$BASE_DIR/gt_pdb
 
 
 
-
 #*DIRECT REAL *****************************************************
 BASE_DIR="../cryofold/particlesSNR1.0"
-RUN_DIR=$BASE_DIR/run_test
+RUN_DIR=$BASE_DIR/bench_cryofomer_optim_large
 python ./scripts/train.py $BASE_DIR/Particles/particles.mrcs  \
     --poses $BASE_DIR/particles.pkl \
     --ctf $BASE_DIR/ctf.pkl \
     -n 100 \
     -o $RUN_DIR \
     --pixel_size 2.2 \
-    --sigma 2.5 \
+    --sigma 1.05 \
     --quality_ratio 5.0 \
     --embedding_path ../cryofold/embeddings/4ake_A_embeddings.pt \
     --initial_pose_path $BASE_DIR/initial_pose.pt \
     --af_checkpoint_path  ../openfold/openfold/resources/openfold_params/finetuning_no_templ_1.pt \
-    --batch-size 2  \
+    --batch-size 16  \
     --num-workers 0 \
     --zdim 4  \
     --enc-dim 64 \
     --enc-layers 4 \
-    --dec-dim 96 \
-    --dec-layers 2 \
+    --dec-dim 32 \
+    --dec-layers 4 \
     --domain real \
     --encode-mode conv \
     --overwrite\
-    --pair_stack
+    --all_atom \
+    --pair_stack\
+    --frozen_structure_module\
+    --wd 1e-4\
+    --lr 5e-4\
+    --warmup 100\
     
 python ./scripts/analyze.py  -o $RUN_DIR/analysis $RUN_DIR 99 --pc 2 --trajectory
 python ./scripts/compare_traj.py "$RUN_DIR/analysis/traj/*pdb" "$BASE_DIR/gt_pdbs/*pdb" $RUN_DIR/analysis/stats.txt
